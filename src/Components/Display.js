47e51866bea0eb7cf281bb06608e3a0d8d7e3e7d
Nacho <nachoherrero93@hotmail.com>
2020-08-15 19:41:00 -0300

    Fix App.css

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -4,7 +4,7 @@ function Machine(props) {
   return (
     <div>
       <cite className="text" id="text">{props.state.newQuote}</cite>
-      <p className="author" id="author">-{props.state.newAuthor}</p>
+      <p className="author" id="author">- {props.state.newAuthor}</p>
     </div>
   );
 }
